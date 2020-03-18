047dea929219b63bdd0e8e045bdf8e562c772df9
AuTa <auta520@live.com>
2020-03-18 11:21:23 +0800

    feat: add 35
    二分查找

---
+++ leetcode.35.search-insert-position/search_insert_position-binary-search.go
@@ -0,0 +1,70 @@
+/*
+ * @lc app=leetcode id=35 lang=golang
+ *
+ * [35] Search Insert Position
+ *
+ * https://leetcode.com/problems/search-insert-position/description/
+ *
+ * algorithms
+ * Easy (41.44%)
+ * Likes:    1880
+ * Dislikes: 225
+ * Total Accepted:    532.5K
+ * Total Submissions: 1.3M
+ * Testcase Example:  '[1,3,5,6]\n5'
+ *
+ * Given a sorted array and a target value, return the index if the target is
+ * found. If not, return the index where it would be if it were inserted in
+ * order.
+ *
+ * You may assume no duplicates in the array.
+ *
+ * Example 1:
+ *
+ *
+ * Input: [1,3,5,6], 5
+ * Output: 2
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: [1,3,5,6], 2
+ * Output: 1
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: [1,3,5,6], 7
+ * Output: 4
+ *
+ *
+ * Example 4:
+ *
+ *
+ * Input: [1,3,5,6], 0
+ * Output: 0
+ *
+ *
+ */
+package leetcode35
+
+// @lc code=start
+func searchInsert(nums []int, target int) int {
+	var low, high = 0, len(nums) - 1
+	for low <= high {
+		mid := (low + high) >> 1
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] > target {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+	return low
+}
+
+// @lc code=end
